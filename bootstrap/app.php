d21c8b3a2ef95e2ccf8fdab1724756a4ec8f7c78
miuki-rgb <lukisn7890@gmail.com>
2026-01-30 18:45:51 +0700

    fix target class admin error dan rigister middleware alias

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->trustProxies(at: '*');
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
